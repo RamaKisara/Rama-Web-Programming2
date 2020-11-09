56b4f592c943a45070cb6a1104dd9927155909c9
ramakisara <whocares@yahoo.com>
2020-11-09 11:36:45 +0700

    Pustaka Booking Beserta Pratikum

---
+++ Pratikum/praktikum1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php 
+class Perkenalan extends CI_Controller{
+	public function index(){
+		echo "<h1>Perkenalkan Nama Saya<h1>";
+		echo "<h2>Rama Kisara<br>
+			Saya tinggal didaerah Brebes<br>
+			Hobi saya adalah airsoft<h2>";
+	}
+}
+
+
---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,22 @@
+        <section>
+            <h1><?php echo $judul ?></h1>
+            <h4>Nama</h4>
+            <ul type="disc">
+                <li>Nama Depan : Rama</li>
+                <li>Nama Belakang : Kisara</li>
+            </ul>
+            <br>
+            <h4>Alamat</h4>
+            <ul type="none">
+                <li> [REDACTED]</li>
+            </ul>
+            <h4>Tempat Lahir</h4>
+            <ul type="none">
+                <li>Tarakan</li>
+            </ul>	
+            <h4>Olah Raga Favorit</h4>
+            <ul type="square">
+                <li>American Football</li>
+                <li>Catur</li>
+            </ul>	
+        </section>
